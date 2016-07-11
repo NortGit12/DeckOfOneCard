3d1cf0d7ff7bd08e45e72a4923e65a634e5309c1
Jeff Norton <jeff12ntn@gmail.com>
2016-07-11 11:27:38 -0600

    Implemented NetworkController

---
+++ DeckOfOneCard/CardController.swift
@@ -0,0 +1,25 @@
+//
+//  CardController.swift
+//  DeckOfOneCard
+//
+//  Created by Jeff Norton on 7/11/16.
+//  Copyright © 2016 JCN. All rights reserved.
+//
+
+import Foundation
+
+class CardController {
+    
+    // MARK: - Stored Properties
+    
+    static let baseURL = NSURL(string: "http://deckofcardsapi.com/api/deck/new/draw/")
+    
+    // MARK: - Method(s)
+    
+    static func drawCards(numberOfCards: Int, completion: ((card: [Card]) -> Void)) {
+        
+        
+        
+    }
+    
+}
